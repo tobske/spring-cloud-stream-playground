ee3571b5ae02cab6af70aac1855b0bf2414ea35c
tobske <tobske@posteo.de>
2020-10-10 09:12:44 +0200

    feat: first empty message producer spring project

---
+++ message-producer/src/test/kotlin/io/tobske/springcloudstreamplayground/messageproducer/MessageProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.tobske.springcloudstreamplayground.messageproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
